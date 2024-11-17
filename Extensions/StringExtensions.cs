bb23ec6de3c1486da97c39a45bf1d1aa43f4225e
Thinspeed <offmailno5@gmail.com>
2024-11-17 16:24:03 +0300

    Add string extensions

---
+++ Extensions/StringExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace Extensions;
+
+public static class StringExtensions
+{
+    public static string ConvertToPascalCase(this string s)
+    {
+        if (string.IsNullOrWhiteSpace(s)) return string.Empty;
+
+        int startIndex = 0;
+        if (s.StartsWith('_'))
+        {
+            if (s.Length == 1) return s;
+            
+            startIndex = 1;
+        }
+        
+        return char.ToUpper(s[startIndex]) + s.Substring(startIndex + 1);
+    }
+}
