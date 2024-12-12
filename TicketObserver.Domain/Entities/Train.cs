18e30e9689594844cb11d4ba0e9ef28ab7f6cff9
Thinspeed <offmailno5@gmail.com>
2024-12-12 13:04:16 +0300

    Add public constructor

--- TicketObserver.Domain/Entities/Train.cs
+++ TicketObserver.Domain/Entities/Train.cs
@@ -6,6 +6,12 @@ namespace TicketObserver.Domain.Entities;
 [EfConstructor]
 public partial class Train : Entity
 {
+    public Train(string number, DateTime startDate)
+    {
+        Number = number ?? throw new ArgumentNullException(nameof(number));
+        StartDate = startDate;
+    }
+    
     public string Number { get; set; }
     
     public DateTime StartDate { get; set; }
